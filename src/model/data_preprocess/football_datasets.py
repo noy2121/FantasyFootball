1181dbadd97c9c3d5ad3d8d25d1c01512ac4db57
Noy <noy.rahmani@viisights.com>
2024-05-27 14:56:35 +0300

    create a dataset class to handle data manipulation and changes. create text_df methods for games and player_valuations

---
+++ src/model/data_preprocess/football_datasets.py
@@ -0,0 +1,101 @@
+
+import numpy as np
+import pandas as pd
+
+
+class FootballDataset:
+
+    def __init__(self, name, dfs):
+
+        self.dfs = dfs
+        self.df = dfs[name]
+        self.name = name
+        self.columns = self.df.columns.tolist()
+
+    def create_text_df_for_valuations(self):
+        """
+        for each row in the df the corresponding row in the text_df will be
+        "first name: fname, lname: last_name date: date, market value in euro: market_value_in_euro, club: club"
+        """
+        rows = []
+        for i, row in self.df.iterrows():
+            pid = row['player_id']
+            first_name = self.dfs['players'].loc[self.dfs['players']['player_id'] == pid]['first_name'].values[0]
+            last_name = self.dfs['players'].loc[self.dfs['players']['player_id'] == pid]['last_name'].values[0]
+
+            text = f'first name: {first_name}, last name: {last_name}, '
+            text += ", ".join([f'{" ".join(c.split("_"))}: {row[c]}' for c in self.columns[1:-1]])
+            rows.append(text)
+
+        text_df = pd.DataFrame(data=rows, columns=['text'])
+
+        return text_df
+
+    def create_text_df_for_players(self):
+        pass
+
+    def create_text_df_for_appearances(self):
+        pass
+
+    def create_text_df_for_games(self):
+        """
+        for each row in the df the corresponding row in the text_df will be
+        "competition: c_name, season: s, round: r, date: date, home club: home club name, away club: away club name,
+        home goals: hg, away goals: ag, home club position: hcp, away club position: acp, home club manager: hcm,
+        away club manager: acm, stadium: s, attendance: a, referee: ref, home club formation: hcf,
+        away club formation: acf, competition type: ct"
+        """
+
+        competition_map = self.dfs['competitions'].set_index('competition_id')['name'].to_dict()
+        rows = []
+        for i, row in self.df.iterrows():
+            d = {
+                'competition_id': competition_map[row['competition_id']],
+                'competition_type': row['competition_type'],
+                'season': row['season'],
+                'round': row['round'],
+                'date': row['date'],
+                'home_club_name': row['home_club_name'],
+                'away_club_name': row['away_club_name'],
+                'home_goals': row['home_goals'],
+                'away_goals': row['away_goals'],
+                'home_club_position': row['home_club_position'],
+                'away_club_position': row['away_club_position'],
+                'home_club_manager': row['home_club_manager'],
+                'away_club_manager': row['away_club_manager'],
+                'stadium': row['stadium'],
+                'attendance': row['attendance'],
+                'referee': row['referee'],
+                'home_club_formation': row['home_club_formation'],
+                'away_club_formation': row['away_club_formation']
+            }
+            text = ', '.join([f'{" ".join(k.split("_"))}: {v}' for k, v in d.items()])
+            rows.append(text)
+
+        text_df = pd.DataFrame(data=rows, columns=['text'])
+
+        return text_df
+
+    def create_text_df(self):
+        """
+        create a text_df for each df with shape (n_rows, 1) containing only text, so we could fine tune the
+        model on sentences and not tabular data
+
+        :return: pd.DataFrame in the corresponding format
+        """
+
+        if self.name == 'player_valuations':
+            return self.create_text_df_for_valuations()
+        elif self.name == 'players':
+            return self.create_text_df_for_players()
+        elif self.name == 'appearances':
+            return self.create_text_df_for_appearances()
+        elif self.name == 'games':
+            return self.create_text_df_for_games()
+
+    def __str__(self):
+        return f'{self.name}\n{self.df.head(10)}'
+
+    def __len__(self):
+        return len(self.df.shape[0])
+

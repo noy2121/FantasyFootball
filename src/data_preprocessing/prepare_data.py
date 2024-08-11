eef6b0cf6017a6cf03841b812ead096647784145
Noy <noy.rahmani@viisights.com>
2024-08-11 19:35:46 +0300

    - change func 'filter_data_by_club_id' to handle numerous columns - create func fo adding 'period' column - add games and events df creators - split cards event to yellow/second/red

--- src/data_preprocessing/prepare_data.py
+++ src/data_preprocessing/prepare_data.py
@@ -2,15 +2,17 @@ import os
 from pathlib import Path
 from functools import reduce
 from typing import List, Dict
+from datetime import datetime
 
 # import sys
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pandas as pd
+import numpy as np
 from unidecode import unidecode
 
-from datasets_structure import teams, events_cols, matches_cols
-from src.utils.utils import get_root_dir, set_random_seed
+from datasets_structure import teams, events_cols, games_cols
+from src.utils.utils import get_root_dir, set_random_seed, save_dataframes
 
 ROOT_DIR = get_root_dir()
 
@@ -19,7 +21,7 @@ def get_relevant_club_ids(df: pd.DataFrame) -> List[int]:
     """
     Retrieve relevant club IDs based on the club names matching the predefined teams list.
     """
-    assert df['club_name'].tolist() == teams
+    assert set(df['club_name']) == teams, f'Clubs in the DataFrame are different in {teams} !'
     return df['club_id'].tolist()
 
 
@@ -31,11 +33,12 @@ def filter_data_by_year(df: pd.DataFrame, year: int) -> pd.DataFrame:
     return df.query("@query")
 
 
-def filter_data_by_club_id(df: pd.DataFrame, colname: str, club_ids: List[int]) -> pd.DataFrame:
+def filter_data_by_club_id(df: pd.DataFrame, colnames: List[str], club_ids: List[int]) -> pd.DataFrame:
     """
     Filter DataFrame by club IDs.
     """
-    return df[df[colname].isin(club_ids)]
+    mask = df[colnames].isin(club_ids).all(axis=1)
+    return df[mask]
 
 
 def fix_name_format(df: pd.DataFrame, colname: str) -> pd.DataFrame:
@@ -43,6 +46,19 @@ def fix_name_format(df: pd.DataFrame, colname: str) -> pd.DataFrame:
     Standardize the name format by removing accents and special characters.
     """
     df[colname] = df[colname].apply(unidecode)
+    return df
+
+
+def add_period_to_df(df: pd.DataFrame, start: int, end: int) -> pd.DataFrame:
+    """
+    Adds "period" column to dataframe for better date filtering.
+    """
+    years = end - start
+    df['date'] = pd.to_datetime(df['date'])
+    bins = [pd.Timestamp(f'{start + i}-08-01') for i in range(years + 1)]
+    labels = [f'{(start + i) % 100}/{(start + i + 1) % 100}' for i in range(years)]
+    df['period'] = pd.cut(df['date'], bins=bins, labels=labels)
+
     return df
 
 
@@ -123,19 +139,45 @@ def get_lineups(lineups_df: pd.DataFrame, players_df: pd.DataFrame, year: int) -
     return pd.merge(players_df, lineups_sum_df, on='player_id', how='left')
 
 
-def create_players_df(dfs: Dict[str, pd.DataFrame], club_ids: List[int], start_year: int, out_dir: str):
+def split_cards_events(events_df: pd.DataFrame) -> pd.DataFrame:
     """
-    Create and save a comprehensive players DataFrame.
+    Split 'Cards' event type to 'yellow-card', 'red-card', 'second-yellow-card' event types.
     """
-    print('Create players dataframe...')
+    new_events_df = events_df.copy()
+
+    cards_mask = new_events_df['type'] == 'Cards'
+    cards_df = new_events_df[cards_mask]
+
+    yellows = cards_df[cards_df['description'].str.contains('yellow', case=False, na=False) &
+                       ~cards_df['description'].str.contains('second yellow', case=False, na=False)].copy()
+    second_yellows = cards_df[cards_df['description'].str.contains('second yellow', case=False, na=False)].copy()
+    reds = cards_df[cards_df['description'].str.contains('red', case=False, na=False)].copy()
+
+    assert (yellows['type'] == 'Cards').all(), f'Some yellow card events have wrong event type !'
+    assert (second_yellows['type'] == 'Cards').all(), f'Some second yellow card events have wrong event type !'
+    assert (reds['type'] == 'Cards').all(), f'Some red card events have wrong event type !'
+
+    # change event type Cards -> yellow/second/red
+    yellows['type'] = 'yellow-card'
+    second_yellows['type'] = 'second-yellow-card'
+    reds['type'] = 'red-card'
+
+    new_cards = pd.concat([yellows, second_yellows, reds])
+    new_events_df = pd.concat([new_events_df[~cards_mask], new_cards]).reset_index(drop=True)
+    new_events_df.rename(columns={'game_event_id': 'event_id', 'type': 'event_type'})
+
+    return new_events_df
+
+
+def create_players_df(dfs: Dict[str, pd.DataFrame], club_ids: List[int], start_year: int, curr_year: int) -> pd.DataFrame:
+    """
+    Create a comprehensive players DataFrame.
+    """
+    print('Extract players data...')
 
     app_df = filter_data_by_year(dfs['appearances'], start_year)
-    app_df = filter_data_by_club_id(app_df, 'player_current_club_id', club_ids)
-    app_df['date'] = pd.to_datetime(app_df['date'])
-
-    bins = [pd.Timestamp(f'{start_year + i}-08-01') for i in range(6)]
-    labels = [f'{(start_year + i) % 100}/{(start_year + i + 1) % 100}' for i in range(5)]
-    app_df['period'] = pd.cut(app_df['date'], bins=bins, labels=labels)
+    app_df = filter_data_by_club_id(app_df, ['player_current_club_id'], club_ids)
+    app_df = add_period_to_df(app_df, start_year, curr_year)
 
     stats_dfs = []
     for colname in ['goals', 'assists', 'red_cards', 'yellow_cards']:
@@ -147,8 +189,50 @@ def create_players_df(dfs: Dict[str, pd.DataFrame], club_ids: List[int], start_y
     players_df = get_lineups(dfs['game_lineups'], players_df, start_year)
     players_df = fix_name_format(players_df, 'player_name')
 
-    print("Save players DataFrame...")
-    players_df.to_csv(f'{out_dir}/players_df.csv', index=False)
+    return players_df
+
+
+def create_games_df(raw_games_df: pd.DataFrame, club_ids: List[int], start_year: int, curr_year: int) -> pd.DataFrame:
+    """
+    Create a comprehensive games DataFrame.
+    """
+    print('Extract games data...')
+
+    games_df = filter_data_by_year(raw_games_df, start_year)
+    games_df = filter_data_by_club_id(games_df, ['home_club_id', 'away_club_id'], club_ids)
+    games_df = add_period_to_df(games_df, start_year, curr_year)
+
+    return games_df[games_cols]
+
+
+def create_events_df(raw_events_df: pd.DataFrame, games_df: pd.DataFrame, start_year: int) -> pd.DataFrame:
+    print('Extract events data...')
+    # filter by game_id
+    # no need to filter by year or club_id, games_df has already been filtered this way
+    relevant_game_ids = games_df['game_id'].tolist()
+    events_df = raw_events_df[raw_events_df['game_id'].isin(relevant_game_ids)]
+
+    events_df['date'] = pd.to_datetime(events_df['date'])
+    cutoff_date = pd.Timestamp(f'{start_year}-08-01')
+    assert (events_df['date'] >= cutoff_date).all(), f'Some dates in the DataFrame are before {cutoff_date}'
+
+    events_df = split_cards_events(events_df)
+    events_df = events_df[events_cols]
+
+    return events_df
+
+
+def prepare_dataframes(raw_dfs: Dict[str, pd.DataFrame], club_ids: List[int], start_year: int, curr_year: int, out_dir: str):
+
+    # create players df
+    players_df = create_players_df(raw_dfs, club_ids, start_year, curr_year)
+    games_df = create_games_df(raw_dfs['games'], club_ids, start_year, curr_year)
+    events_df = create_events_df(raw_dfs['game_events'], games_df, start_year)
+
+    print("Save DataFrames...")
+    filepaths = ['players_data', 'games_data', 'events_data']
+    filepaths = [f'{out_dir}/{fp}.csv' for fp in filepaths]
+    save_dataframes([players_df, games_df, events_df], filepaths)
 
 
 def preprocess_data():
@@ -163,14 +247,14 @@ def preprocess_data():
             filepath = os.path.join(data_dir, filename)
             dataframes[filename[:-4]] = pd.read_csv(filepath)
 
-    dataframes['clubs'] = pd.read_csv(cfg.clubs_df_filepath)
+    out_dir = os.path.join(ROOT_DIR, 'data/csvs')
+    dataframes['clubs'] = pd.read_csv(f'{out_dir}/clubs_data.csv')
     relevant_club_ids = get_relevant_club_ids(dataframes['clubs'])
 
-    starting_year = 2019
-    out_dir = os.path.join(ROOT_DIR, 'data/csvs')
-    Path(out_dir).mkdir(parents=True, exist_ok=True)
+    start_year = 2019
+    current_year = datetime.now().year
 
-    create_players_df(dataframes, relevant_club_ids, starting_year, out_dir)
+    prepare_dataframes(dataframes, relevant_club_ids, start_year, current_year, out_dir)
 
 
 if __name__ == '__main__':
